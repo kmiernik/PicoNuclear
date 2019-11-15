9d7c6f3b87b8a6dcb9fe63fc89d7cfcac77c7293
Krzysztof Miernik <k.a.miernik@gmail.com>
2019-11-15 21:57:18 +0100

    Initial commit
    After moving to PicoNuclear module from single file.

---
+++ README.txt
@@ -0,0 +1,25 @@
+===========
+PicoNuclear
+===========
+
+...
+
+Instalation
+============
+
+This package requires the following modules:
+* numpy (http://www.numpy.org/)
+* matplotlib (http://matplotlib.org/, 
+              https://github.com/matplotlib/matplotlib)
+* scipy 
+* picosdk-python-wrappers
+       
+Ones the required libraries are in place, install the Pyspectr with:
+    python3 setup.py build
+    sudo python3 setup.py install
+    or 
+    pip install ...
+
+Usage
+=====
+
---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='PicoNuclear',
+    version='0.1.0',
+    author='Krzysztof Miernik',
+    author_email='kamiernik@gmail.com',
+    packages=['PicoNuclear'],
+    url=['https://github.com/kmiernik/PicoNuclear'],
+    scripts=['bin/mini_pet.py',
+             'bin/pico_capture.py'],
+    license='LICENSE.txt',
+    description='Front-end for picoscope to be used for nuclear physics\
+            related projects setup',
+    long_description=open('README.txt').read(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+    install_requires=['matplotlib', 'numpy', 'scipy', 'picosdk']
+)
