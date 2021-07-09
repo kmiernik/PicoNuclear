ef2eb32a836fa2181de0b71d49459b302aeda1ff
Krzysztof Miernik <k.a.miernik@gmail.com>
2021-07-09 08:14:52 +0200

    Midwork on package distrubution

--- setup.py
+++ setup.py
@@ -1,28 +1,32 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.txt", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='PicoNuclear',
     version='0.1.0',
     author='Krzysztof Miernik',
     author_email='kamiernik@gmail.com',
-    packages=['PicoNuclear'],
-    package_dir={'PicoNuclear': ''},
-    package_data={'PicoNuclear': ['config/*.xml'],
-                  'PicoNuclear': ['demo/*.txt'],
-                },
-    url=['https://github.com/kmiernik/PicoNuclear'],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_dir={'PicoNuclear':'PicoNuclear'},
+    #package_data={'': ['config/*.xml'],
+                  #'': ['demo/*.txt'],
+                #},
     scripts=['bin/mini_pet.py',
              'bin/miniPET.py',
              'bin/pico_capture.py'],
     license='LICENSE.txt',
     description='Front-end for picoscope to be used for nuclear physics\
             related projects setup',
-    long_description=open('README.txt').read(),
+    long_description=long_description,
+    url=['https://github.com/kmiernik/PicoNuclear'],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering :: Physics",
     ],
     install_requires=['matplotlib', 'numpy', 'scipy', 'picosdk']
